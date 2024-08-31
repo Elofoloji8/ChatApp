da5e81d51a3508143ef463d2b8050be0a1311122
tunaybasturk <74294028+tunaybasturk@users.noreply.github.com>
2024-08-31 20:56:47 +0300

    - Icon added and comment numbers on the welcome page have been removed.

--- app/src/main/java/com/example/chatapp/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -18,13 +18,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ChatAppTheme {
+            ChatAppTheme{
                 AppNavigation()
-                /*deneme2*/
-
-                /*deneme1*/
-                // deneme 3/
-                // DENEME 4
             }
         }
     }
